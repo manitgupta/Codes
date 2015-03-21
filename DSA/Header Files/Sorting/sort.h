4a1803ad62c088cec20909196fb6f11c6e39d25b
Manit Gupta <manitgupta95@gmail.com>
2015-03-21 17:23:04 +0530

    Changed Folder Organization. More Organized and More Code.

---
+++ DSA/Header Files/Sorting/sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+void quicksort(int *a, int p, int r);
+void insertion_sort(int *a, int N);
+void merge(int *a,int l, int m, int r);
+void mergesort(int *a,int l,int r);
+
+#endif
