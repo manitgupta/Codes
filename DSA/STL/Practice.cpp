de331fced422b494b74c60737f990383c68f408d
Manit Gupta <manitgupta95@gmail.com>
2015-07-29 12:27:59 +0530

    STL Tutorial on TopCoder

---
+++ DSA/STL/Practice.cpp
@@ -0,0 +1,55 @@
+// @BEGIN_OF_SOURCE_CODE
+ 
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <bitset>
+#include <utility>
+#include <set>
+#include <numeric>
+#include <ctime>
+ 
+#define Inf 2147483647
+#define Pi acos(-1.0)
+#define N 1000000
+#define LL long long
+ 
+inline LL Power(int b, int p) { LL ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+const int dr [] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dc [] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+#define F(i, a) for( int i = (0); i < (a); i++ )
+#define Fs(i, sz) for( size_t i = 0; i < sz.size (); i++ )
+#define Fe(i, x) for(typeof (x.begin()) i = x.begin(); i != x.end (); i++)
+#define Set(a, s) memset(a, s, sizeof (a))
+#define Max(a, b)  (a < b ? b : a)
+#define Min(a, b)  (a > b ? b : a)
+ 
+using namespace std;
+ 
+int main() 
+{
+    std::vector<int> v;
+    for (int i = 0; i < 100; ++i)
+    {
+     	v.push_back(i*i);
+    }
+    //Use *max_element for getting the value.
+    int value = *max_element(v.begin(), v.end());
+    printf("%d\n",value);
+    //Subtract the begin iterator of a container to get index.
+    int index = max_element(v.begin(), v.end()) - v.begin();
+    printf("%d\n",index);
+
+    return 0;   
+}
